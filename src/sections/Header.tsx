55406f09e4d576a4fb8a58856a749a4a6d680d41
Sujeeth Shingade <sujeethshingade04@gmail.com>
2025-01-18 21:38:21 +0530

    add header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,6 +1,5 @@
 import { FC } from "react";
 
-/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 const navItems = [
   {
     label: "About",
@@ -25,7 +24,28 @@ const navItems = [
 ];
 
 const Header: FC = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container !max-w-full">
+        <div className="flex justify-between h-20 items-center">
+          <div>
+            <a href="/">
+              <span className="font-bold uppercase text-xl">Alex Taylor</span>
+            </a>
+          </div>
+          <div className="flex items-center gap-4">
+            <div className="border border-stone-400 size-11 rounded-full inline-flex items-center justify-center">
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect x="3" y="15" width="18" height="2" fill="currentColor" />
+                <rect x="3" y="7" width="18" height="2" fill="currentColor" />
+              </svg>
+            </div>
+            <button className="bg-red-orange-500 h-11 px-6 text-white rounded-xl border border-red-orange-500 uppercase hidden md:block">Contact Me</button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,9 +7,27 @@ const config: Config = {
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
+        screens: {
+            sm: "375px",
+            md: "768px",
+            lg: "1024px",
+        },
         extend: {
             fontFamily: {
                 sans: "var(--font-archivo)",
+            },
+            container: {
+                center: true,
+                padding: {
+                    DEFAULT: '1rem',
+                    md: '2rem',
+                    lg: '4rem',
+                }
+            },
+            colors: {
+                "red-orange": {
+                    500: 'color-mix(in srgb, theme("colors.red.500") 50%, theme("colors.orange.500") 50%)',
+                }
             },
         },
     },
