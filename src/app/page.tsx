a37431af91b6d6a73e94d20bf164781367967d66
Sujeeth Shingade <sujeethshingade04@gmail.com>
2025-01-18 21:52:27 +0530

    add intro section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/sections/Header";
+import Intro from "@/sections/Intro";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Intro />
     </>
   );
 }
