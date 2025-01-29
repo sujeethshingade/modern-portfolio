be3c65b03fedb5ec353ac7928f99aab755dc2ca9
Sujeeth Shingade <sujeethshingade04@gmail.com>
2025-01-29 14:48:11 +0530

    add testimonials section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/sections/Header";
 import Hero from "@/sections/Hero";
 import Intro from "@/sections/Intro";
 import Projects from "@/sections/Projects";
+import Testimonials from "@/sections/Testimonials";
 import FAQs from "@/sections/FAQs";
 import Footer from "@/sections/Footer";
 
@@ -12,6 +13,7 @@ export default function Home() {
       <Hero />
       <Intro />
       <Projects />
+      <Testimonials />
       <FAQs />
       <Footer />
     </>
