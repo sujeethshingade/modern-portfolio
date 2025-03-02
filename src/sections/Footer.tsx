2fec8b1dfdb5df6a51fea7b1b88b8b7fc8505bd7
Sujeeth Shingade <sujeethshingade04@gmail.com>
2025-03-02 23:46:47 +0530

    animate footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,51 +1,81 @@
+"use client";
+
 import Button from "@/components/Button";
-import { FC } from "react";
+import { useInView } from "motion/react";
+import { FC, useEffect } from "react";
+import useTextRevealAnimation from "@/hooks/useTestRevealAnimation";
 
 const navItems = [
   {
-    href: '#',
-    label: 'Home'
+    href: '#intro',
+    label: 'About',
   },
   {
-    href: '#',
-    label: 'Projects'
+    href: '#projects',
+    label: 'Projects',
   },
   {
-    href: '#',
-    label: 'Testimonials'
+    href: '#testimonials',
+    label: 'Testimonials',
   },
   {
-    href: '#',
-    label: 'Faqs'
+    href: '#faqs',
+    label: 'Faqs',
   },
   {
-    href: '#',
-    label: 'Contact'
+    href: '#contact',
+    label: 'Contact',
   }
 ]
 
 const Footer: FC = () => {
+
+  const { scope, entranceAnimation } = useTextRevealAnimation();
+  const inView = useInView(scope);
+
+  useEffect(() => {
+    if (inView) {
+      entranceAnimation();
+    }
+  }, [inView, entranceAnimation]);
+
+  const handleClickNavItem = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    const url = new URL(e.currentTarget.href);
+    const hash = url.hash;
+    const target = document.querySelector(hash);
+    if (!target) return;
+    target.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <footer className="bg-stone-900 text-stone-200">
       <div className="container" id="contact">
         <div className="section">
           <div className="flex items-center gap-3">
-            <div className="size-3 rounded-full bg-green-400"></div>
+            <div className="size-3 rounded-full bg-green-500 animate-pulse"></div>
             <span className="uppercase">
               One spot available for next month
             </span>
           </div>
           <div className="grid md:grid-cols-3 md:items-center">
             <div className="md:col-span-2">
-              <h2 className="text-4xl md:text-7xl lg:text-8xl mt-8 font-extralight">
+              <h2 className="text-4xl md:text-7xl lg:text-8xl mt-8 font-extralight" ref={scope}>
                 Enough talk. Let&apos;s make something great together.
               </h2>
               <a href="mailto:info@alextaylor.com">
                 <Button varient="secondary" className="mt-8"
                   iconAfter={
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-                    </svg>
+                    <div className="size-6 overflow-hidden">
+                      <div className="w-12 h-6 flex group-hover/button:-translate-x-1/2 transition-transform duration-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                        </svg>
+                      </div>
+                    </div>
                   }>
                   info@alextaylor.com
                 </Button>
@@ -54,7 +84,7 @@ const Footer: FC = () => {
             <div className="md:col-span-1">
               <nav className="flex flex-col md:items-end gap-8 mt-16 md:mt-0">
                 {navItems.map(({ href, label }) => (
-                  <a href={href} key={label}>
+                  <a href={href} key={label} onClick={handleClickNavItem}>
                     <Button varient="text" className="text-lg">{label}</Button>
                   </a>
                 ))}
